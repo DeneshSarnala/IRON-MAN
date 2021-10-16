0cc17ac96546e8ee612c31fcb814aceececefffa
DeneshSarnala <92620454+DeneshSarnala@users.noreply.github.com>
2021-10-16 12:06:46 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+Hello World
+
+----------+++++-------
+This is Denesh Sarnala 
+----------+++++-------
+
+Dont Be Sad 
+Be Happy 
+
+by a middle class boy 
+
+h           h 
+h           h 
+h h h h h h h 
+h           h 
+h           h
